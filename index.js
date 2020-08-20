0e56b93abb102daa9d38ed7ad7ab7c7d998f6cc3
Dilshan <dopatha@nc.rr.com>
2020-08-20 19:48:02 -0400

    final push

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+var mysql = require("mysql");
 var inquirer = require("inquirer");
+var util = require("util");
 require("console.table");
 require('dotenv').config()
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.myPassword,
+  database: "employeeTrackerDB"
+});
+
+connection.query = util.promisify(connection.query);
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected as ID " + connection.threadId);
   start();
 });
+
 
 function start() {
 inquirer
